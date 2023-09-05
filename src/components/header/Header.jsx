f06c8692b18bc17f209300dd9ddf532e0cc79067
OkraGrey <hasnainsohail15@gmail.com>
2023-09-05 15:42:12 +0500

    Add Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import "./header.css";
+const Header = () => {
+  return (
+    <>
+      <div className="nav_container">
+        <div className="logo">
+          <a href="#">LS</a>
+        </div>
+        <div className="elements">
+          <a href="#">About Us</a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
